7bfda476d92058b142ecdbf3d95acb76f803a1c4
mostafa-meerzad <mostafameerzad@gmail.com>
2025-11-19 18:06:10 +0430

    frontend: add estedad font and shadcn

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
-
+// import {} from "../public/fonts/es"
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -16,10 +17,32 @@ export const metadata = {
   description: "پلتفرمی برای پیدا کردن کتاب‌های مورد علاقه‌تان",
 };
 
+const estedad = localFont({
+  src: [
+    {
+      path: "../public/fonts/estedad/Estedad-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/estedad/Estedad-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../public/fonts/estedad/Estedad-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-estedad",
+});
+
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="fa">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="fa" dir="rtl">
+      <body className={`${estedad.variable} ${geistSans.variable} ${geistMono.variable} max-w-[1440px] mx-auto font-estedad`}>
         <main>{children}</main>
       </body>
     </html>
