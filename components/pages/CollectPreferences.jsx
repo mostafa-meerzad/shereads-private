781f716e900dd4b11bedbc31b105be951c21fb13
mostafa-meerzad <mostafameerzad@gmail.com>
2025-11-21 15:26:42 +0430

    frontend: complete collect preferences page

---
+++ components/pages/CollectPreferences.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React from "react";
+import logo from "@/assets/logo.png";
+import { Spinner } from "../ui/shadcn-io/spinner";
+import LoadingTexts from "../ui/LoadingText";
+
+const CollectPreferences = ({ onComplete }) => {
+  return (
+    <div className="relative flex justify-center items-center p-6 lg:p-10 bg-[url('/sign-up-img.png')] bg-cover min-h-screen w-full">
+      <div className="absolute inset-0 bg-white opacity-60 bg-center"></div>
+      <Image
+        src={logo}
+        width={200}
+        height={50}
+        alt="logo"
+        className="h-6 w-fit absolute top-6 lg:top-10 left-6 lg:left-10"
+      />
+      <div className="flex flex-col justify-center items-center text-green-700 gap-28">
+        <LoadingTexts onComplete={onComplete} />
+        {/* animation / loading */}
+        <Spinner variant={"circle-filled"} size={68} />
+      </div>
+    </div>
+  );
+};
+
+export default CollectPreferences;
