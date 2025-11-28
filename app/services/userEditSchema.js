969221b62bdf7ef4361c18b8e8ca00dac5007ff1
elyasbromand <151540092+elyasbromand@users.noreply.github.com>
2025-11-28 20:38:33 +0430

    Update role

--- app/services/userEditSchema.js
+++ app/services/userEditSchema.js
@@ -1,10 +1,11 @@
 import z from "zod";
-import { AgeEnum, GenderEnum } from "./answersSchema";
+import { AgeEnum, GenderEnum, RoleEnum } from "./answersSchema";
 
 export const UserEditSchema = z.object({
   name: z.string().min(1).optional(),
   gender: GenderEnum.optional(), 
   email: z.string().email().optional(),
+  role: RoleEnum.optional().default('user'),
   password: z.string().min(6).optional(),
   Age: AgeEnum.optional().nullable(),
   isActive: z.boolean().nullable().optional(),
